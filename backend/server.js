a238599a42df34de9b6451ec6aaa3ed75e35d4f9
gonzalohlh <gonzalo.galdame@gmail.com>
2023-03-02 17:51:57 -0300

    cors coment

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 }; */
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+/* app.use(express.urlencoded({ extended: false })); */
 /* app.use(
   cors({ origin: ["http://localhost:3000/", "https://comida-ja.onrender.com"] })
 ); */
