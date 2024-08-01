53777b049a1b966a09ae55dcd1b56abeb82ddf75
gonzalohlh <gonzalo.galdame@gmail.com>
2024-08-01 17:03:33 -0300

    git ignore

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGO_UR);
+    const connect = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected`);
   } catch (error) {
     console.log(error);
@@ -10,4 +10,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
+module.exports = connectDB; // este es el otro metodo para conectarse a MongoDB
