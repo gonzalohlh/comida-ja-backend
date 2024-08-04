80376e29333928ef2b68f2d23b35d46b935adff0
gonzalohlh <gonzalo.galdame@gmail.com>
2024-08-04 12:08:38 -0300

    cambio tamaño color entregado

--- backend/models/nameModel.js
+++ backend/models/nameModel.js
@@ -6,6 +6,10 @@ const namesSchema = mongoose.Schema(
       type: String,
       required: [true, "Agregar un nombre"],
     },
+    /* amount: {
+      type: Number,
+      required: [true, "Agregar una cantidad"],
+    }, */
     completed: {
       type: Boolean,
       required: true,
@@ -20,3 +24,25 @@ const namesSchema = mongoose.Schema(
 const Names = mongoose.model("Names", namesSchema);
 
 module.exports = Names;
+
+/*opcion mejora:
+{
+    name: {
+      type: String,
+      required: [true, "Agregar un nombre"],
+    },
+    payment: {
+      type: Number,
+      required: [true, "Agregar un monto"],
+    },
+    amount: {
+      type: Number,
+      required: [true, "Agregar una cantidad"],
+    },
+    completed: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  }
+*/
